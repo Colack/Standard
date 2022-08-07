0905148596703e4c98b2d8980c8878beb7a1161b
Jack Spencer <UniformKitten281@gmail.com>
2022-08-06 19:37:29 -0700

    `Update Stringy.js || 🚚`

--- src/values/Stringy.js
+++ src/values/Stringy.js
@@ -1,7 +1,9 @@
 /*
-  @Varrience
-  Provides some polyfills and new functionality to basic strings left out by CDO
+  Completed by @Varrience
+  Notes:
+       - Provides some polyfills and new functionality to basic strings left out by CDO
 */
+
 // Repeats string casing
 String.prototype.repeat = function (num) {
     var newString = this;
@@ -10,20 +12,24 @@ String.prototype.repeat = function (num) {
     }
     return (newString);
 }
+
 // Inserts a string within a string at a given position
 String.prototype.insert = function (str, pos?) {
     return (this.substring(0, pos) + str + this.substring(pos, this.length));
 }
+
 // Checks beginning
 String.prototype.startsWith = function (search, index) {
     index = index === undefined || index > this.length ? 0 : index
     return this.substring(index, search.length) === search;
 }
+
 // Checks ending
 String.prototype.endsWith = function (search, index) {
     index = index === undefined || index > this.length ? this.length : index
     return this.substring(index - search.length, index) === search;
 };
+
 // Reverses the entireity of a string
 String.prototype.reverse = function () {
     var reverse = "";
@@ -32,6 +38,7 @@ String.prototype.reverse = function () {
     }
     return (reverse);
 }
+
 // Basically useless like why would you?
 String.prototype.anagram = function () {
     var old = this.split("");
@@ -43,6 +50,7 @@ String.prototype.anagram = function () {
     }
     return (str);
 }
+
 // Pads the beginning of a string with a value
 String.prototype.padStart = function (num, val) {
     var start = ""
@@ -54,6 +62,7 @@ String.prototype.padStart = function (num, val) {
     start += this;
     return (start);
 }
+
 // Pads the ending of a string
 String.prototype.padEnd = function (num, val) {
     var end = this;
