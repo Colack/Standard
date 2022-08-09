a0e276faf639e543b344cd95a2327c26599a057b
Jack Spencer <UniformKitten281@gmail.com>
2022-08-08 20:44:41 -0700

    `Mock Values`

---
+++ src/values/mocking.js
@@ -0,0 +1,33 @@
+/*
+  Completed by @Colack (8/8/22)
+  Notes:
+    - This libary is for testing different values and 'mocking' them.
+*/
+
+var expections = [];
+
+// Send a expected message, then the actual message to send.
+String.prototype.expect = function(expected, message) {
+	expections.push({
+		expected: expected,
+		actual: message
+	});
+};
+
+// Mock a message by sending a value.
+String.prototype.mock = function(message) {
+	if (message === undefined) {
+		return this;
+	} else {
+		for (var i = 0; i < expections.length; i++) {
+			if (expections[i].expected === message) {
+				return expections[i].actual;
+			}
+		}
+	}
+}
+
+// Clear the expected values.
+String.prototype.clear = function() {
+	expections = [];
+}
