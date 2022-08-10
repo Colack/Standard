1385242c0594c1af87905931e52362969f57c418
Varrience <105261606+Varrience@users.noreply.github.com>
2022-08-09 23:39:17 -0400

    math.js
    comments updated

--- src/values/math.js
+++ src/values/math.js
@@ -31,37 +31,37 @@ Number.prototype.modulo = function(n) {
 	return this % n;
 };
 
-// useful in cases where a variable may not be a number
+// Useful in cases where a variable may not be a number
 Number.isInteger = function (value) {
     return (typeof value === 'number' && isFinite(value) && Math.floor(value) === value)
 }
 
-// checks if a given value is a float
+// Checks if a given value is a float
 Number.isFloat = function (value) {
     return (!Number.isInteger(value) && isFinite(value));
 }
 
-// returns the lowest value possible of a positive or negative number
+// Returns the lowest value possible of a positive or negative number
 Number.prototype.redline = function () {
     return (Math.abs(this) - Math.abs(this - 1));
 }
 
-// is more shorthanded just in case the type is already a number and need specific testing
+// Is more shorthanded just in case the type is already a number and need specific testing
 Number.prototype.isInteger = function () {
     return (typeof this === 'number' && isFinite(this) && Math.floor(this) === this)
 }
 
-// shorthanded version if the datatype is already a Number
+// Shorthanded version if the datatype is already a Number
 Number.prototype.isFloat = function () {
     return (!this.isInteger() && isFinite(this));
 }
 
-// checks if a(n) number is even or odd (floats & doubles will be treated as odd)
+// Checks if a(n) number is even
 Number.prototype.isEven = function () {
     return (this % 2 === 0);
 }
 
-// Checks if a number is even
+// Checks if a number is odd
 Number.prototype.isOdd = function () {
     return (!this.isEven());
 }
@@ -77,31 +77,31 @@ Number.prototype.isPrime = function () {
     return (this > 1);
 }
 
-// checks if number only contains a decimal value
+// Checks if number only contains a decimal value
 Number.prototype.isDecimal = function () {
     return (this > -1 && this < 1 && this !== 0);
 }
 
-// returns a decimal from a float or double
+// Returns a decimal from a float or double
 Number.prototype.getDecimal = function () {
     var str = this.toString();
     var pos = str.indexOf(".");
     return (parseFloat(str.substring(pos, str.length)));
 }
 
-// floors at a specific decimal place
+// Floors at a specific decimal place
 Number.prototype.floorAt = function (point) {
     var p = Math.pow(10, point);
     return (Math.floor(this * p) / p || Math.floor(this))
 }
 
-// rounds at a specific decimal place 
+// Rounds at a specific decimal place 
 Number.prototype.roundAt = function (point) {
     var p = Math.pow(10, point);
     return (Math.round(this * p) / p || Math.round(this))
 }
 
-// ceils at a specific decimal place
+// Ceils at a specific decimal place
 Number.prototype.ceilAt = function (point) {
     var p = Math.pow(10, point);
     return (Math.ceil(this * p) / p || Math.ceil(this))
