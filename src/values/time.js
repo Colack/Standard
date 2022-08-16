46455a3c190b45bf54377f65955f2ecffa982830
Jack Spencer <UniformKitten281@gmail.com>
2022-08-16 08:21:54 -0700

    Create time.js

---
+++ src/values/time.js
@@ -0,0 +1,25 @@
+/*
+  Completed By Colack (8/16/22)
+*/
+
+// Format and Return a new date
+function formatDate() {
+  var d = new Date();
+  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var day = days[d.getDay()];
+  var hr = d.getHours();
+  var min = d.getMinutes();
+  if (min < 10) {
+    min = "0" + min;
+  }
+  var ampm = "am";
+  if (hr > 12) {
+    hr -= 12;
+    ampm = "pm";
+  }
+  var date = d.getDate();
+  var month = months[d.getMonth()];
+  var year = d.getFullYear();
+  return day + " " + hr + ":" + min + ampm + " " + date + " " + month + " " + year;
+}
