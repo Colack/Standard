077a0766266d2b682ba64fb79bfe964bee2ab33c
Jack Spencer <UniformKitten281@gmail.com>
2022-08-17 19:31:50 -0700

    Create credits.js

---
+++ src/other/credits.js
@@ -0,0 +1,20 @@
+/*
+    @Author @Colack
+    @Date 08/17/2022
+    @About
+     - This file contains all credits from the './CREDITS' file. Thanks for all the help!
+*/
+
+function credits() {
+    console.log("CDO Standard Libary");
+    console.log("Version: 1.0.0");
+    console.log("Author: Many People");
+    console.log("");
+    console.log("Thanks to:");
+    console.log("- Colack");
+    console.log("- Varrience");
+    console.log("Everyone at [WUT] World, and the GameLab forum. :)");
+    console.log("");
+    console.log("Thank you for using this library!");
+    console.log(":)");
+}
