e3c9c6f65a7f3a7ddbccccdaf7ea1c48ba7644cb
Jack Spencer <UniformKitten281@gmail.com>
2022-08-17 19:18:47 -0700

    Update vals.js

--- src/values/vals.js
+++ src/values/vals.js
@@ -1,9 +1,8 @@
 /*
-    Completed by Colack (8/3/2022)
-    Notes:
-        - This file is actually pretty Useful!
-        - Note to self:
-            - Make sure to use the correct syntax for each type of variable.
+    @Authors @Colack
+     - Last updated by @Colack  (8/17/22)
+    @About
+     - Library for messing around with values.
 */
 
 // Set a variable to its default value
