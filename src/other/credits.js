50cca6b1d99b88a1acefe315a6276a9299482c77
Varrience <105261606+Varrience@users.noreply.github.com>
2022-08-17 23:41:10 -0400

    1 console log statement
    multiple console log statements can be condensed into 1 line although i formatted it to fit multiple lines so it doesn't look as bad

--- src/other/credits.js
+++ src/other/credits.js
@@ -1,20 +1,18 @@
 /*
-    @Author @Colack
+    @Author @Colack, @Varrience
     @Date 08/17/2022
     @About
      - This file contains all credits from the './CREDITS' file. Thanks for all the help!
 */
 
 function credits() {
-    console.log("CDO Standard Libary");
-    console.log("Version: 1.0.0");
-    console.log("Author: Many People");
-    console.log("");
-    console.log("Thanks to:");
-    console.log("- Colack");
-    console.log("- Varrience");
-    console.log("Everyone at [WUT] World, and the GameLab forum. :)");
-    console.log("");
-    console.log("Thank you for using this library!");
-    console.log(":)");
+    console.log("CDO Standard Libary\n\
+Version: 1.0.0\n\
+Author: Many People\n\n\
+Thanks to:\n\
+- Colack\n\
+- Varrience\n\
+Everyone at [WUT] World, and the GameLab forum. :)\n\n\
+Thank you for using this library!\n\
+:)");
 }
