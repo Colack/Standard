110ded2db3d4293bbbdfc151e87def233db32e2c
Varrience <105261606+Varrience@users.noreply.github.com>
2022-08-22 16:55:50 -0400

    Fatal Error Fix
    idk what you were planning to do... i thought the 1st iteration was good enough, I've provided 2 working functions that execute the credits differently, perhaps pick the one that you like or build off of it

--- src/other/credits.js
+++ src/other/credits.js
@@ -15,15 +15,39 @@ var credits = [
     'Everyone at [WUT] World, and the GameLab forum.',
     'Thank you for using this library!'
 ];
-
+// Is this what you were trying to accoplish here? wait each message
+function roll() {
+    var index = 0;
+    var loop = setInterval(function(){
+    if(index < credits.length){
+        console.log(credits[index]);
+        index++;
+    } else {
+        clearInterval(loop);
+    }
+},waitTime);
+}
+// Or is this what you planned for?
+function roll2() {
+    setTimeout(function(){
+        for(var i = 0; i < credits.length; i++){
+            console.log(credits[i]);
+        }
+    },waitTime)
+}
+/*
+Fatal Error?? Duplicate identifier names will crash this
 function credits() {
     var x = false;
     for (var i = 0; i < credits.length; i++) {
         if (x) {
             console.log(credits[i]);
+            // Are you waiting a certian amount of time to run each message? or is this just individual delay?
             setTimeout(function() {
                 x = true;
+                // No function recall?
             }, waitTime);
         } 
     }
 }
+*/
