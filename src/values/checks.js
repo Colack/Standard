d3062e9870e908a2e28b6a5b406a75151ed9ec75
Varrience <105261606+Varrience@users.noreply.github.com>
2022-08-22 22:09:28 -0400

    move to array.js?
    this seems like it should be in a file that was already created, wondering if it should be moved?

--- src/values/checks.js
+++ src/values/checks.js
@@ -5,11 +5,11 @@
     - Library for checking values for specific things
 */
 
-// Check two arrays for the same value
-function arrCheck(ar1, ar2, callback) {
-    for (var i = 0; i < ar1.length; i++) {
-        for (var j = 0; j < ar2.length; i++) {
-            if (ar1[i] == ar2[i]) {
+// Check two arrays (for the same value) <- too vauge (if they match at least once)
+Array.prototype.arrCheck(arr, callback) {
+    for (var i = 0; i < this.length; i++) {
+        for (var j = 0; j < arr.length; i++) {
+            if (this[i] === arr[i]) { // if were doing exact matches === is better
                 callback();
                 return true;
             }
