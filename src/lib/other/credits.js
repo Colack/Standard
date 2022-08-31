9613231b991c59069f9787e627f663c80784fc4b
Jack Spencer <UniformKitten281@gmail.com>
2022-08-31 10:40:48 -0700

    Update credits.js

--- src/lib/other/credits.js
+++ src/lib/other/credits.js
@@ -1,6 +1,6 @@
 /*
-    @Author @Colack @Varrience
-    @Date 08/21/2022
+    @Authors @Colack @Varrience
+     - Last Updated by Colack   (8/31/22)
     @About
      - This file contains all credits from the './CREDITS' file. Thanks for all the help!
 */
@@ -15,39 +15,9 @@ var credits = [
     'Everyone at [WUT] World, and the GameLab forum.',
     'Thank you for using this library!'
 ];
-// Is this what you were trying to accoplish here? wait each message
-function roll() {
-    var index = 0;
-    var loop = setInterval(function(){
-    if(index < credits.length){
-        console.log(credits[index]);
-        index++;
-    } else {
-        clearInterval(loop);
-    }
-},waitTime);
-}
-// Or is this what you planned for?
-function roll2() {
-    setTimeout(function(){
-        for(var i = 0; i < credits.length; i++){
-            console.log(credits[i]);
-        }
-    },waitTime)
-}
-/*
-Fatal Error?? Duplicate identifier names will crash this
+
 function credits() {
-    var x = false;
     for (var i = 0; i < credits.length; i++) {
-        if (x) {
-            console.log(credits[i]);
-            // Are you waiting a certian amount of time to run each message? or is this just individual delay?
-            setTimeout(function() {
-                x = true;
-                // No function recall?
-            }, waitTime);
-        } 
+        console.log(credits[i]);
     }
 }
-*/
