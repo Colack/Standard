d63b8e9d73e7a5fe75668d3b5b5023e75cdd9219
Jack Spencer <UniformKitten281@gmail.com>
2022-08-31 10:42:40 -0700

    Update checks.js

--- src/lib/values/checks.js
+++ src/lib/values/checks.js
@@ -1,18 +1,23 @@
 /*
     @Authors @Colack
-    - Last updated by Colack    (8/22/22)
+    - Last updated by @Colack    (8/31/22)
     @About
     - Library for checking values for specific things
 */
 
-// Check two arrays (for the same value) <- too vauge (if they match at least once)
+// Check two arrays (for the same value) at least once
 Array.prototype.arrCheck(arr, callback) {
     for (var i = 0; i < this.length; i++) {
         for (var j = 0; j < arr.length; i++) {
-            if (this[i] === arr[i]) { // if were doing exact matches === is better
+            if (this[i] === arr[i]) {
                 callback();
                 return true;
             }
         }
     }
+}
+
+// Contributors
+function contributors() {
+    console.log("@Colack");
 }
