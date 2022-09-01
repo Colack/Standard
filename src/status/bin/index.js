8fbd4175a10d4cbf9228869f93f4d50da536658c
Jack Spencer <UniformKitten281@gmail.com>
2022-09-01 08:07:46 -0700

    Update index.js

--- src/status/bin/index.js
+++ src/status/bin/index.js
@@ -1,12 +1,13 @@
 /*
     @Authors @Colack @Monster_YT
-    - Last updated by @Colack   (8/26/22)
+    - Last updated by @Colack   (9/1/22)
     @About
     - Set your discord status to a code.org project!
 */
 
 const clientID = "1011782808676606042"
 const url = "https://studio.code.org/v3/channels/" + process.argv[2];
+const isGame = process.argv[4];
 
 const client = require('discord-rich-presence')(clientID);
 const fetch = require('node-fetch');
@@ -50,64 +51,62 @@ function timerCounter() {
 }
 
 const start = async function() {
-    if (process.argv[2] != "colack") {
-        const response = await fetch(url);
-        const data = await response.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
-        const ver = await fetch('https://colack.github.io/verified.json');
-        const verdata = await ver.json();
+    const ver = await fetch('https://colack.github.io/verified.json');
+    const verdata = await ver.json();
+    if (data) {
+        projectName = data.name;
+        projectType = data.projectType;
 
-        if (data) {
-            projectName = data.name;
-            projectType = data.projectType;
+        if(data.publishedAt==null){pubd="Not published."}else{pubd=data.id}
 
-            if(data.publishedAt==null){pubd="Not published."}else{pubd=data.id}
+        if(projectType=="gamelab"){pic="js"; type="Javascript"}
 
-            if(projectType=="gamelab"){SIK="js"; SIT="Javascript"}
-
-            if (process.argv[3] != undefined) {
-                if (process.argv[3] == "js") {
-                    pic = "js";
-                    type = "JavaScript";
-                } else if (process.argv[3] == "block") {
-                    pic = "block";
-                    type = "Blocks";
-                } else {
-                    pic = "js";
-                    type = "JavaScript";
-                }
+        if (process.argv[3] != undefined) {
+            if (process.argv[3] == "js") {
+                pic = "js";
+                type = "JavaScript";
+            } else if (process.argv[3] == "block") {
+                pic = "block";
+                type = "Blocks";
             } else {
                 pic = "js";
                 type = "JavaScript";
             }
-
-            if(projectType=="gamelab"){projectType="Game Lab";}
-            if(projectType=="applab"){projectType="App Lab";}
-            if(projectType=="playlab"){projectType="Play Lab"; type="Blocks"; pic="block"}
-            if(projectType=="dancelab"){projectType="Dance Lab"; type="Blocks"; pic="block"}
-
-            thumbnail = "https://studio.code.org" + data.thumbnailUrl;
-            stateDetails = projectName;
-            stateDescription = "Project Type: " + projectType;
-
-            for (var i = 0; i < verdata.verified.length; i++) {
-                if (verdata.verified[i] == process.argv[2]) {
-                    pic = "verified";
-                    type = "Verified Project";
-                    stateDetails = projectName + " ✅";
-                    stateDescription = "Verified Project";
-                }
-            }
-
-            count = setInterval(timerCounter, 1000);
+        } else {
+            pic = "js";
+            type = "JavaScript";
         }
-    } else {
-        type = "Emerald Splash";
-        thumbnail = "kak";
-        pubd = "Lero";
-        stateDetails = "Stopping Dio";
-        stateDescription = "Cairo, Egypt";
-        pic = "green";
+
+        if(projectType=="gamelab"){projectType="Game Lab";}
+        if(projectType=="applab"){projectType="App Lab";}
+        if(projectType=="playlab"){projectType="Play Lab"; type="Blocks"; pic="block"}
+        if(projectType=="dancelab"){projectType="Dance Lab"; type="Blocks"; pic="block"}
+
+        thumbnail = "https://studio.code.org" + data.thumbnailUrl;
+        stateDescription = "Project Type: " + projectType;
+
+        if (isGame) {
+            stateDetails = "Playing " + projectName;
+        } else {
+            stateDetails = "Working on " + projectName;
+        }
+
+        for (var i = 0; i < verdata.verified.length; i++) {
+            if (verdata.verified[i] == process.argv[2]) {
+                pic = "verified";
+                type = "Verified Project";
+                if (isGame) {
+                    stateDetails = "Playing" + projectName + " ✅";
+                } else {
+                    stateDetails = "Working on " + projectName + " ✅";
+                }
+                stateDescription = "Verified " + projectType;
+            }
+        }
+
         count = setInterval(timerCounter, 1000);
     }
 }
