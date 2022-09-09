1022d5dcf6de4be5ccc9134c05d27bd87389bae5
Varrience <105261606+Varrience@users.noreply.github.com>
2022-09-09 00:07:59 -0400

    Overhaul optimization of formatDate()

--- src/lib/values/time.js
+++ src/lib/values/time.js
@@ -1,11 +1,12 @@
 /*
-  @Authors @Colack
+  @Authors @Colack @Varrience
    - Last updated by @Colack    (8/31/22)
   @About
    - Library for updating, receiving, and formating time values.
 */
 
 // Format and Return a new date
+/* Date does this already
 function formatDate() {
   var d = new Date();
   var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -26,7 +27,28 @@ function formatDate() {
   var year = d.getFullYear();
   return day + " " + hr + ":" + min + ampm + " " + date + " " + month + " " + year;
 }
-
+*/
+// not sure if mine is faster but some of the other stuff can be taken out if your not interested
+function formatDate(millitary?: boolean) {
+    let d = Date();
+    let hour = Number(d.substring(16, 18));
+    let ampm = "";
+    if (millitary && this.hour > 12) {
+        hour -= 12;
+        ampm = "PM";
+    } else {
+        hour += 12;
+        ampm = "AM";
+    }
+    let minute = d.substring(19, 21)
+    let second = d.substring(22, 24);
+    let day = d.substring(0, 3);
+    let date = d.substring(8, 10);
+    let month = d.substring(4, 7);
+    let year = d.substring(11, 15);
+    let zone = d.substring(28,33)
+    return (`${day} ${hour}:${minute} ${ampm} ${date} ${month} ${year}`)
+}
 // Contributors
 function contributors() {
     console.log("@Colack");
