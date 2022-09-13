9a1f863c33d2f0bcf28fcc24b3163a1b724a332e
Jack Spencer <UniformKitten281@gmail.com>
2022-09-12 19:06:48 -0700

    Update elements.js

--- src/applab/display/elements.js
+++ src/applab/display/elements.js
@@ -1,3 +1,10 @@
+/*
+    @Authors @Colack
+        - Last Updated by Colack    (9/12/22)
+    @About
+        Create Elements using javascript
+ */
+
 /* 
   Creates a button on the screen displaying the text provided and referenced by the given id.
   id (string)
@@ -16,4 +23,9 @@ function mkbutton(id, text, x, y, width, height, border) {
   
   return 0;
   
+}
+
+// Contributors
+function contributors() {
+    console.log("@Colack");
 }
