dade47a847a9939df600089e8d977e17e6e19f26
Varrience <105261606+Varrience@users.noreply.github.com>
2022-09-18 18:21:39 -0400

    reworking arrCheck
    when I first looked over this it seemed fine until I looked at it again and man there was a lot of stuff that needed to be fixed, the biggest one being that the function wasn't even declared to begin with rendering it useless to a newcomer who wishes to just copy paste functions and learn later unless ease of access was not our goal?

--- src/applab/values/checks.js
+++ src/applab/values/checks.js
@@ -1,20 +1,23 @@
 /*
-    @Authors @Colack
+    @Authors @Colack @Varrience
     - Last updated by @Colack    (8/31/22)
+    - Last updated by @Varrience    (9/18/22)
     @About
     - Library for checking values for specific things
 */
 
 // Check two arrays (for the same value) at least once
-Array.prototype.arrCheck(arr, callback) {
+// Bad declaration this typeof polyfill does not exist as you know calling an unregistered function is like asking for an error
+Array.prototype.arrCheck = function(arr=[], callback=function(){}) { // adding defaults here fix your issue of not giving a value to them
     for (var i = 0; i < this.length; i++) {
         for (var j = 0; j < arr.length; i++) {
             if (this[i] === arr[i]) {
-                callback();
-                return true;
+                callback(); // this is also bad what if i decide not to include a callback the result? => undefined() which would error
+                return(true);
             }
         }
     }
+    return(false); // just noticed this now... why this wasn't added? if no matches are found and it expects a boolean this should be included
 }
 
 // Contributors
